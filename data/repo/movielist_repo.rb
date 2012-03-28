915d3b1775b2fd9f4c3cc930b56021ed044f1b40
Jim Ecker <jim@ecker.com>
2012-03-28 00:11:08 -0400

    movie list changes

---
+++ data/repo/movielist_repo.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+require "sinatra/base"
+require 'net/https'
+require 'json'
+require "./lib/logging"
+
+class MovieListRepo 
+  include Sinatra::Logging
+
+  def initialize
+  end
+
+  def GetAll
+	movieList = MovieItem.all
+	movieList
+  end
+
+  def SaveMovie(movie)
+	 raise "movie must be a MovieItem" unless movie.kind_of? MovieItem
+	 info "Save the movie: #{movie.Title}"
+	 movie.save
+  end
+
+end
+
---
+++ lib/dbconnect.rb
@@ -0,0 +1,18 @@
+module QuickieDBConnect
+
+  def MongoConnectDev
+	databaseEnv = 'localhost'
+	MongoMapper.connection = Mongo::Connection.new(databaseEnv)
+	MongoMapper.database = "delbookmarks"
+  end
+
+  def MongoConnectProd
+	databaseEnv = ENV['MONGOHQ_URL']
+	MongoMapper.connection = Mongo::Connection.from_uri(databaseEnv)
+	buffer = databaseEnv.split("\/")
+	MongoMapper.database = buffer[-1]
+  end
+
+  module_function :MongoConnectDev
+  module_function :MongoConnectProd
+end
